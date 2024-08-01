c8794ccbba5461dcbe7f864688126f0eea3082e6
Suraj Bathija <108985041+surajvfx@users.noreply.github.com>
2024-08-01 11:06:44 +0100

    Update NullCreator.py

--- NullCreator.py
+++ NullCreator.py
@@ -2,15 +2,14 @@
 Tool Name: Out Null Creator v1.0
 Author: Suraj Bathija
 Tool Description: 1. Asks for User Preferences (first launch only) and saves it into a text file.
-                  2. Modifies an exisiting selected null based on saved User Preferences.
-                  3. If selected node is not a null, Creates an OUT Null and connects to last selected node based on saved User Preferences.
-                  
-Note: Preferences are saved in Documents folder as a .txt file and can be deleted to re-save new user prefs.
+                  2. Creates an OUT Null on last selected node if it's not a Null Node based on saved User Preferences.
+                  3. Modifies an exisiting selected null based on saved User Preferences.
+
+Note: Preferences are saved in User folder as a .txt file and can be deleted to re-save new user prefs
 
 Suggestion: Under Hotkeys, Set Network Pane Shortcut to numpad 0 or any other available shorcut key
             for quick access to the tool
 """
-
 import hou
 import os
 import time
@@ -34,64 +33,65 @@ shapes = editor.nodeShapes()
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__(None, QtCore.Qt.WindowStaysOnTopHint)
-        self.setWindowTitle("Out Null Creator v1.3")
-        self.setGeometry(100, 100, 400, 780)
+        self.setWindowTitle("Out Null Creator v1.0")
  
         layout = QtWidgets.QVBoxLayout()
+        layout.setSpacing(10)
+        layout.setMargin(15)
         self.setLayout(layout)
         
         # Group Box to select color from a pallette
         self.color_box = QtWidgets.QGroupBox("Select Color", self)
-        self.color_box.setGeometry(10, 10, 380, 280)
+        self.color_box.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
 
         color_layout = QtWidgets.QHBoxLayout()
         self.color_box.setLayout(color_layout)
+        layout.addWidget(self.color_box)
 
-        self.palette = hou.qt.ColorPalette(size=56,columns=4, rows=4)
-        self.palette.setGeometry(10, 20, 40, 40)
+        self.palette = hou.qt.ColorPalette(size=64,columns=4, rows=4)
+        self.palette.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         color_layout.addWidget(self.palette)
         self.palette.colorSelected.connect(self.chosen_color)
         
         # Group box to add a prefix
         self.group_box = QtWidgets.QGroupBox("Add Prefix", self)
         self.group_box.setCheckable(True)
-        self.group_box.setGeometry(10, 300, 380, 100)
+        layout.addWidget(self.group_box)
         
         group_layout = QtWidgets.QHBoxLayout()
         self.group_box.setLayout(group_layout)
         self.name_prefix = QtWidgets.QLineEdit()
-        group_layout.addWidget(self.name_prefix)
+        self.name_prefix.setMinimumSize(80,30)
+        group_layout.addWidget(self.name_prefix,stretch=3)
         self.name_prefix.setText("OUT_")
         
         # Group box for selecting a shape for Null
         self.shape_box = QtWidgets.QGroupBox("Select Null Shape", self)
         self.shape_box.setCheckable(False)
-        self.shape_box.setGeometry(10, 410, 380, 100)
+        
+        shapebox_layout = QtWidgets.QHBoxLayout()
+        self.shape_box.setLayout(shapebox_layout)
+        layout.addWidget(self.shape_box)
 
         # Combo Box for Null shapes
         self.comboBox = QtWidgets.QComboBox(self)
-        self.comboBox.setGeometry(QtCore.QRect(30, 450, 340, 40))
         self.comboBox.setObjectName("comboBox")
+        shapebox_layout.addWidget(self.comboBox)
+        self.comboBox.setMinimumSize(80,40)
 
         for shape in shapes:
             self.comboBox.addItem(shape)
             
         self.comboBox.setCurrentIndex(6)
-        
-        #Check Box for creating a corresponding Object Merge
-        
-        self.check_box = QtWidgets.QGroupBox("Select Object Merge Shape", self)
-        self.check_box.setCheckable(False)
-        self.check_box.setGeometry(10, 520, 380, 140)
-           
-        self.check = QtWidgets.QCheckBox("Create Corresponding Object Merge", self)
-        self.check.setGeometry(30, 610, 380, 40)
-        self.check.stateChanged.connect(self.checkBox_status)
 
         #Combo Box for Object Merge shapes
+        merge_layout = QtWidgets.QVBoxLayout()
+        merge_layout.setSpacing(10)
+        
         self.mergeBox = QtWidgets.QComboBox(self)
-        self.mergeBox.setGeometry(QtCore.QRect(30, 560, 340, 40))
         self.mergeBox.setObjectName("mergeBox")
+        self.mergeBox.setMinimumSize(80,40)
+        merge_layout.addWidget(self.mergeBox)
 
         for shape in shapes:
             self.mergeBox.addItem(shape)
@@ -101,17 +101,30 @@ class MainWindow(QtWidgets.QWidget):
         
         global mergeBox
         mergeBox = self.mergeBox
+        
+        #Check Box for creating a corresponding Object Merge
+        
+        self.check_box = QtWidgets.QGroupBox("Select Object Merge Shape", self)
+        self.check_box.setCheckable(False)
+        
+        self.check_box.setLayout(merge_layout)
+        layout.addWidget(self.check_box)
+           
+        self.check = QtWidgets.QCheckBox("Create Corresponding Object Merge", self)
+        self.check.stateChanged.connect(self.checkBox_status)
+        merge_layout.addWidget(self.check)
 
         # Button Transference
         self.transfer_button = QtWidgets.QPushButton("Save preferences to File", self)
-        self.transfer_button.setGeometry(10, 670, 380, 40)
+        self.transfer_button.setMinimumSize(80, 40)
         self.transfer_button.clicked.connect(self.savePrefs)
         self.transfer_button.clicked.connect(self.notifyUser)
+        layout.addWidget(self.transfer_button)
     
         # Progress Bar
         self.progress_bar = QtWidgets.QProgressBar(self)
-        self.progress_bar.setGeometry(15, 725, 380, 35)
         self.progress_bar.setValue(0)
+        layout.addWidget(self.progress_bar)
         
     # Check create merge checkbox and set state    
     def checkBox_status(self, state):
