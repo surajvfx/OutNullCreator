5195536252f488e8c25c9340d4e0ec6852da04b5
Suraj Bathija <108985041+surajvfx@users.noreply.github.com>
2024-08-03 01:11:36 +0100

    Update NullCreator.py
    Added additional comments and cleaned up function and variable names. Also initialised variables with default value to prevent errors to user.

--- NullCreator.py
+++ NullCreator.py
@@ -23,7 +23,7 @@ global shapes
 usr = str(os.path.expanduser("~"))
 docs = (usr+"/Documents")
 txt = str('outNullUserPref.txt')
-prefs = docs+"\\"+txt
+prefs = docs+"/"+txt
 
 # Get Node Shapes and store them into shapes global variable
 editor = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
@@ -31,110 +31,136 @@ shapes = editor.nodeShapes()
 
 # Define Class MainWindow as QWidget which will hold all UI
 class MainWindow(QtWidgets.QWidget):
+
     def __init__(self):
         super(MainWindow, self).__init__(None, QtCore.Qt.WindowStaysOnTopHint)
         self.setWindowTitle("Out Null Creator v1.0")
+        self.setMaximumSize(400, 800)
  
+        # Create vertical box layout for window
         layout = QtWidgets.QVBoxLayout()
         layout.setSpacing(10)
         layout.setMargin(15)
-        self.setLayout(layout)
+        self.setLayout(layout) 
         
-        # Group Box to select color from a pallette
+        # Group Box to select color from a pallette and add widget to window layout 
         self.color_box = QtWidgets.QGroupBox("Select Color", self)
         self.color_box.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
-
+        layout.addWidget(self.color_box)
+        
+        # Create horizontal box widget layout which contains color palette
         color_layout = QtWidgets.QHBoxLayout()
         self.color_box.setLayout(color_layout)
-        layout.addWidget(self.color_box)
 
+        # Create color palette and add widget to color layout
         self.palette = hou.qt.ColorPalette(size=64,columns=4, rows=4)
         self.palette.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         color_layout.addWidget(self.palette)
         self.palette.colorSelected.connect(self.chosen_color)
         
-        # Group box to add a prefix
-        self.group_box = QtWidgets.QGroupBox("Add Prefix", self)
-        self.group_box.setCheckable(True)
-        layout.addWidget(self.group_box)
+        # Group box to add a prefix and add widget to window layout
+        self.prefix_box = QtWidgets.QGroupBox("Add Prefix", self)
+        self.prefix_box.setCheckable(True)
+        self.prefix_box.setChecked(0)
+        self.prefix_box.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        layout.addWidget(self.prefix_box)
+        self.prefix_box.toggled.connect(self.prefix_value)
         
-        group_layout = QtWidgets.QHBoxLayout()
-        self.group_box.setLayout(group_layout)
+        # Create horizontal box widget layout which contains prefix
+        prefix_layout = QtWidgets.QHBoxLayout()
+        self.prefix_box.setLayout(prefix_layout)
+        
+        # Create QLineEdit to take user input for Prefix
         self.name_prefix = QtWidgets.QLineEdit()
         self.name_prefix.setMinimumSize(80,30)
-        group_layout.addWidget(self.name_prefix,stretch=3)
+        prefix_layout.addWidget(self.name_prefix)
         self.name_prefix.setText("OUT_")
         
-        # Group box for selecting a shape for Null
-        self.shape_box = QtWidgets.QGroupBox("Select Null Shape", self)
-        self.shape_box.setCheckable(False)
+        # Group box for selecting a shape for Null and add widget to window layout
+        self.null_shape_box = QtWidgets.QGroupBox("Select Null Shape", self)
+        self.null_shape_box.setCheckable(False)
+        self.null_shape_box.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        layout.addWidget(self.null_shape_box)
         
-        shapebox_layout = QtWidgets.QHBoxLayout()
-        self.shape_box.setLayout(shapebox_layout)
-        layout.addWidget(self.shape_box)
+        # Create horizontal box widget layout which contains null shape combo box        
+        null_box_layout = QtWidgets.QHBoxLayout()
+        self.null_shape_box.setLayout(null_box_layout)
 
         # Combo Box for Null shapes
-        self.comboBox = QtWidgets.QComboBox(self)
-        self.comboBox.setObjectName("comboBox")
-        shapebox_layout.addWidget(self.comboBox)
-        self.comboBox.setMinimumSize(80,40)
+        self.nullcomboBox = QtWidgets.QComboBox(self)
+        self.nullcomboBox.setObjectName("comboBox")
+        null_box_layout.addWidget(self.nullcomboBox)
+        self.nullcomboBox.setMinimumSize(80,40)
 
         for shape in shapes:
-            self.comboBox.addItem(shape)
+            self.nullcomboBox.addItem(shape)
             
-        self.comboBox.setCurrentIndex(6)
-
-        #Combo Box for Object Merge shapes
-        merge_layout = QtWidgets.QVBoxLayout()
-        merge_layout.setSpacing(10)
+        self.nullcomboBox.setCurrentIndex(6)
         
-        self.mergeBox = QtWidgets.QComboBox(self)
-        self.mergeBox.setObjectName("mergeBox")
-        self.mergeBox.setMinimumSize(80,40)
-        merge_layout.addWidget(self.mergeBox)
-
-        for shape in shapes:
-            self.mergeBox.addItem(shape)
-            
-        self.mergeBox.setCurrentIndex(7)
-        self.mergeBox.setEnabled(False)
-        
-        global mergeBox
-        mergeBox = self.mergeBox
-        
-        #Check Box for creating a corresponding Object Merge
-        
+        # Group Box for Object Merge checkbox and merge shapes combo box
         self.check_box = QtWidgets.QGroupBox("Select Object Merge Shape", self)
         self.check_box.setCheckable(False)
-        
-        self.check_box.setLayout(merge_layout)
+        self.check_box.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
         layout.addWidget(self.check_box)
-           
-        self.check = QtWidgets.QCheckBox("Create Corresponding Object Merge", self)
-        self.check.stateChanged.connect(self.checkBox_status)
-        merge_layout.addWidget(self.check)
+        
+        # Create vertical box widget layout which contains merge shape combo box and check box  
+        merge_box_layout = QtWidgets.QVBoxLayout()
+        merge_box_layout.setSpacing(10)
+        self.check_box.setLayout(merge_box_layout)
 
-        # Button Transference
-        self.transfer_button = QtWidgets.QPushButton("Save preferences to File", self)
-        self.transfer_button.setMinimumSize(80, 40)
-        self.transfer_button.clicked.connect(self.savePrefs)
-        self.transfer_button.clicked.connect(self.notifyUser)
-        layout.addWidget(self.transfer_button)
+        # Combo Box for Object Merge shapes
+        self.mergeComboBox = QtWidgets.QComboBox(self)
+        self.mergeComboBox.setObjectName("mergeBox")
+        self.mergeComboBox.setMinimumSize(80,40)
+        merge_box_layout.addWidget(self.mergeComboBox)
+
+        for shape in shapes:
+            self.mergeComboBox.addItem(shape)
+            
+        self.mergeComboBox.setCurrentIndex(7)
+        self.mergeComboBox.setEnabled(False)
+        
+        global mergeBox
+        mergeBox = self.mergeComboBox
+        
+        # Check Box for creating a corresponding Object Merge
+        self.check = QtWidgets.QCheckBox("Create Corresponding Object Merge", self)
+        self.check.setChecked(0)
+        merge_box_layout.addWidget(self.check)
+        self.check.stateChanged.connect(self.checkBox_status)
+        
+        # Create save button and add to window layout
+        self.save_button = QtWidgets.QPushButton("Save preferences to File", self)
+        self.save_button.setMinimumSize(80, 40)
+        layout.addWidget(self.save_button)
+        
+        self.save_button.clicked.connect(self.savePrefs)
+        self.save_button.clicked.connect(self.notifyUser)
     
-        # Progress Bar
+        # Create save progress Bar and add to window layout
         self.progress_bar = QtWidgets.QProgressBar(self)
         self.progress_bar.setValue(0)
         layout.addWidget(self.progress_bar)
+    
+    # Declare global variable checkBoxState and initialise with value of 0    
+    global checkBoxState    
+    checkBoxState = str("0")
         
     # Check create merge checkbox and set state    
-    def checkBox_status(self, state):
+    def checkBox_status(self, state):     
+        
         global checkBoxState
-        checkBoxState = str(state)
         
         if state > 0:
             mergeBox.setEnabled(True)
+            checkBoxState = str("1")
         else:
             mergeBox.setEnabled(False)
+            checkBoxState = str("0")
+    
+    # Declare global variable colour and initialise with value             
+    global colour
+    colour = str("[64,64,64]")
     
     # Check the user selected color and save it      
     def chosen_color(self, ix, color):
@@ -142,30 +168,39 @@ class MainWindow(QtWidgets.QWidget):
         global colour
         colour = str(selectedColor)
     
-    # Update Progress bar based when user presses save preferences button    
+    # Update Progress bar when user presses save preferences button    
     def update_progress_bar(self):
         self.progress_bar.setValue(0)
         for i in range(101):
             self.progress_bar.setValue(i)
             time.sleep(0.001)  
             QtWidgets.QApplication.processEvents()  
-      
+    
+    # Declare global variable prefix and initialise with no value         
+    global prefix
+    prefix = ""
+    
+    def prefix_value(self):
+        global prefix
+        prefix = str(self.name_prefix.text())       
+    
     #Save preferences
     def savePrefs(self):
-        
-        prefix = str(self.name_prefix.text())
         global selected_shape
-        selected_shape = self.comboBox.currentText()
+        null_shape = self.nullcomboBox.currentText()
         global merge_shape
-        merge_shape = self.mergeBox.currentText()
+        merge_shape = self.mergeComboBox.currentText()
+        
+        parms = (colour+"\n"
+                 +prefix+"\n"
+                 +null_shape+"\n"
+                 +merge_shape+"\n"
+                 +checkBoxState+"\n"
+                )       
 
         #write to text file
         file = open(prefs, 'w')
-        file.write(colour+"\n")
-        file.write(prefix+"\n")
-        file.write(selected_shape+"\n")
-        file.write(merge_shape+"\n")
-        file.write(checkBoxState+"\n")
+        file.write(parms)
         file.close()
 
         self.update_progress_bar()   
@@ -215,21 +250,24 @@ def node_name():
     global user_name
     
     input_name = (hou.ui.readInput('Rename Node', buttons=('OK',), severity=hou.severityType.Message, default_choice=0, close_choice=-1, help=None, title=None))
-    user_name = str.replace(input_name[1], " ", "_")
+    
+    if len(input_name[1]) < 1:
+        user_name = str("null")
+    else:
+        user_name = str.replace(input_name[1], " ", "_")
+    
     final_name = prefix + user_name
     return(final_name)
     return(user_name)
-    print(final_name)
-    print(user_name)
     
-#get last selected node
+#get selected nodes
 select = hou.selectedNodes()
 
 def createNull():
 
     global created_null
 
-    #get first selected node and get the name as string
+    #get last selected node and get the name as string
     node = select[-1]
     nodeName = str(node)
     
@@ -265,8 +303,8 @@ def modifyNull():
 
     global existing_null
 
-    #get first selected node and get the name as string
-    node = select[0]
+    #get last selected node and get the name as string
+    node = select[-1]
     
     #generate name from parent node and apply to null with prefix
     newName = final_name
@@ -284,7 +322,7 @@ def modifyNull():
     
 def createMerge():
 
-    #get first selected node and get the name as string
+    #get last selected node and get the name as string
     node = select[-1]
     nodeName = str(node)
     
